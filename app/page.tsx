84e11a41856f09e9ee1a8c6df5811945d8167c7b
nguyen_anh_khoa <151300565+khoanguyen-dona@users.noreply.github.com>
2025-08-27 16:48:27 +0700

    1 st commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="font-bold text-2xl hover:cursor-pointer">
+      DONAWEBS.COM
+    </div>
+  );
+}
---
+++ app/shoeshop/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>shoeshop</div>
+  )
+}
+
+export default page
